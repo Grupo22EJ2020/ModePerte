324b8df497c3f876261269a5b03fb0e9f9c0309e
DairaT02 <61217693+DairaT02@users.noreply.github.com>
2020-05-27 04:18:16 -0500

    Define clases Empleados

---
+++ Empleados/clases.py
@@ -0,0 +1,28 @@
+from listas import listaEmpleados
+
+class empleado:
+    id = ""
+    nombre = ""
+    edad = 0
+
+def agregarEmpleado():
+    print("---REGISTRO DE EMPLEADO---")
+    nuevoEmpleado = empleado()
+    
+    nuevoEmpleado.id = input("Ingrese ID de empleado:")
+    nuevoEmpleado.nombre = input("Ingrese nombre de empleado:")
+    nuevoEmpleado.edad = input("Ingrese edad de empleado:")
+
+    listaEmpleados.append(nuevoEmpleado);
+
+def borrarEmpleado():
+    id = input("Ingrese ID de empleado a borrar:")
+    listaEmpleados.pop(id)
+
+def modificarEmpleado():
+    print ("modificar empleado")
+
+def consultarEmpleado():
+    print("Lista de todos los empleados:")
+    for a in listaEmpleados:
+        print(a.id," - ", a.nombre, " - ", a.edad)
