9d4509fb70c60f7cda7141c28a8dd8af343c5f9b
Mayela Gonzalez Martinez <mayelagzz99@gmail.com>
2020-05-27 10:58:38 -0500

    Agregue un menu y una opcion

--- Video.py
+++ Video.py
@@ -33,7 +33,21 @@ class Video:
     def fechaPubli (self, valor):
         self.__fechaPubli = valor
 
+    def Menu(self):
+        lista[]
+        while True:
+            try:
+                opcion = int(input("1.Agregar\ 2.Borrar\ 3.Modificar\ 4.Consultar\ 5.Salir\ n.¿Que opcion eliges?:"))
+                if opcion == 1:
+                    self.__idVideo = self.__idVideo+1
+                    self.__nombre = input("Ingresa el nombre del video: ")
+                    self.__url = input("Ingresa la url del video: ")
+                    self.__fechaPubli = input("Ingresa la fecha del video: ")
 
-    
+                    valores = Video(self.__idVideo, self.__nombre, self.__url, self.__fechaPubli)
+                    lista.append(valores)
+                    input("Agregaste un registro, enter para continuar")
+
+
 
     
