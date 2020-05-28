be5cd4b53d3dd9fc2fe2f859970c006ffe4ae827
Mayela Gonzalez Martinez <mayelagzz99@gmail.com>
2020-05-27 20:41:00 -0500

    Se modifico

--- Curso_Tema.py
+++ Curso_Tema.py
@@ -1,3 +1,6 @@
+global listaCursoTema
+listaCursoTema = list()
+
 class Curso_Tema:
     def __init__ (self, idCursoTema, idCurso, idTema):
         self.__idCursoTema = idCursoTema
@@ -23,6 +26,47 @@ class Curso_Tema:
     @idTema.setter
     def idTema (self, valor):
         self.__idTema = valor
+
+def agregarCursoTema():
+    print("---REGISTRO DE CURSOTEMA---")
+
+    Curso_Tema.idCursoTema = input("Ingrese id de cursotema:")
+    Curso_Tema.idCurso = input("Ingrese id de cursotema:")
+    Curso_Tema.idTema = input("Ingrese id de cursotema:")
+
+def borrarCursoTema():
+    id = input("Ingrese id de cursotema a borrar:")
+    listaCursoTema.pop(id)
+
+def modificarCursoTema():
+    print ("modificar cursotema")
+
+def consultarCursoTema():
+    print("Lista de todos los cursotema:")
+    for a in listaCursoTema:
+        print(a.id," - ", a.nombre, " - ", a.direccion)
+
+def menu():    
+    print("1.- Agregar")
+    print("2.- Borrar")
+    print("3.- Modificar")
+    print("4.- Consultar")
+    print("5.- Salir")
+
+
+    print("---CURSOTEMA---")
+    menu()
+    opc = eval(input("Seleccione una opción: "))
+    if opc == 1:
+        agregarCursoTema()
+    elif opc == 2:
+        borrarCursoTema()
+    elif opc == 3:
+        modificarCursoTema()
+    elif opc == 4:
+        consultarCursoTema()
+    
+
 
     
 
