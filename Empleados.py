447ab64f73ef6dcbad7f6f6748c41af3b4b43cc1
DairaT02 <61217693+DairaT02@users.noreply.github.com>
2020-05-27 19:22:42 -0500

    Add propiedades a Empleados

--- Empleados.py
+++ Empleados.py
@@ -2,17 +2,41 @@ global listaEmpleados
 listaEmpleados = list()
 
 class empleado:
-    id = ""
-    nombre = ""
-    direccion = 0
+    def __init__(self, id, nombre, direccion):
+        self.id = id
+        self.nombre = nombre
+        self.direccion = direccion
+
+    @property
+    def id(self):
+        return self.__id
+
+    @id.setter
+    def id(self, valor):
+        self.__id = valor
+
+    @property
+    def nombre(self):
+        return self.__nombre
+
+    @nombre.setter
+    def nombre(self, valor):
+        self.__nombre = valor
+
+    @property
+    def direccion(self):
+        return self.__direccion
+
+    @direccion.setter
+    def direccion(self, valor):
+        self.__direccion = valor
 
 def agregarEmpleado():
     print("---REGISTRO DE EMPLEADO---")
-    nuevoEmpleado = empleado()
-    
-    nuevoEmpleado.id = input("Ingrese ID de empleado:")
-    nuevoEmpleado.nombre = input("Ingrese nombre de empleado:")
-    nuevoEmpleado.direccion = input("Ingrese direccion de empleado:")
+
+    empleado.id = input("Ingrese ID de empleado:")
+    empleado.nombre = input("Ingrese nombre de empleado:")
+    empleado.direccion = input("Ingrese direccion de empleado:")
 
 def borrarEmpleado():
     id = input("Ingrese ID de empleado a borrar:")
